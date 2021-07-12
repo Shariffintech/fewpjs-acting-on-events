5192cba69b20a45e63c61f5ee383ff24d551ce38
Sharifdesigns22 <shariftarver@gmail.com>
2021-07-11 23:58:32 -0400

    commit tree 58194a7aafe432f816836baeb8bbcb4d8f36f081

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+document.addEventListener("keyup", function(e) {
+    if (e.key === "ArrowLeft"){
+        let leftNumbers = dodger.style.left.replace("px","");
+        let left = parseInt(leftNumbers) - 10;
+
+        dodger.style.left = `${left - 1}px`;
+    }
+});
+
+let dogdger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px","");
+    let left = parseInt(leftNumbers,10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+
+}
+
+function moveDodgerRight() {
+    let rightNumbers = dodger.style.right.replace("px","");
+    let right = parseInt(rightNumbers,10);
+
+    if (right < 0) {
+        dodger.style.right = `${right + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function(e) {
+ 
+    if (e.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+});
